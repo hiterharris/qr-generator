2492012407fe1c2f08655f5763586c0b1f98541d
Hiter Harris <hiterharris@comcast.net>
2023-02-22 08:55:07 -0500

    set up repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { copyImageToClipboard } from 'copy-image-clipboard';
+import { FaCheckCircle } from 'react-icons/fa';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [code, setCode] = useState('');
+  const [copied, setCopied] = useState(false);
+
+  const generateCode = (e, url) => {
+    e.preventDefault();
+    fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
+      .then(data => setCode(data?.url))
+      .catch(error => console.log('Error fetching data: ', error))
+    return false;
+  }
+
+  const copyCode = code => {
+    copyImageToClipboard(code);
+    setCopied(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>QR Code Generator</h1>
+      <form onSubmit={(e) => generateCode(e, url)}>
+        <input type="text" placeholder='URL' onChange={e => setUrl(e.target.value)} />
+        <button type="button" onClick={(e) => generateCode(e, url)}>Generate</button>
+      </form>
+      <div className={`code-container ${!code && 'hide'}`} onClick={() => { copyCode(code); setCopied(true) }}>
+        <img src={code} alt='qr code' />
+        {copied && <p>Copied!<FaCheckCircle className='check'/></p>}
+      </div>
     </div>
   );
 }
